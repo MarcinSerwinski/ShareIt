240534e1a5d3bf2cbfcb0d34ab208f60c7049266
MarcinSerwinski <MarcinSerwinski@github.com>
2023-01-09 12:48:54 +0100

    created home_app

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # local apps:
+    'home.apps.HomeConfig',
+
+    # 3rd party apps:
+
 ]
 
 MIDDLEWARE = [

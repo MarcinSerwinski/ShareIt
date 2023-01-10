5583a23f5168e0e0d27d9f4bf25c770b9445e3d2
MarcinSerwinski <MarcinSerwinski@github.com>
2023-01-10 09:27:08 +0100

    main views as TemplateView for testing purpose of templates

--- home/urls.py
+++ home/urls.py
@@ -8,4 +8,6 @@ app_name = 'home'
 urlpatterns = [
     path('', LandingPage.as_view(), name='home'),
     path('add-donation/', AddDonation.as_view(), name='add-donation'),
+    path('login/', Login.as_view(), name='login'),
+    path('register/', Register.as_view(), name='register'),
 ]
--- home/views.py
+++ home/views.py
@@ -7,3 +7,9 @@ class LandingPage(TemplateView):
 
 class AddDonation(TemplateView):
     template_name = 'home/form.html'
+
+class Login(TemplateView):
+    template_name = 'home/login.html'
+
+class Register(TemplateView):
+    template_name = 'home/register.html'

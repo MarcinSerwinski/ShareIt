6d16b35c6121559bff843e3ca5e05a48d2af40d3
MarcinSerwinski <MarcinSerwinski@github.com>
2023-01-11 12:05:35 +0100

    test for institution model in homeapp

--- conftest.py
+++ conftest.py
@@ -6,3 +6,12 @@ from home.models import *
 def create_category(db):
     return Category.objects.create(name='TestNameCategory')
 
+@pytest.fixture()
+def create_institution(db, create_category):
+    institution_types = [(1, 'Fundacja'),
+                         (2, 'Organizacja pozarządowa'),
+                         (3, 'Zbiórka lokalna')]
+    return Institution.objects.create(name='TestNameInstitution',
+                                      description='TestDescriptionInstitution',
+                                      type=institution_types[0][0],
+                                      )
--- home/tests/tests_models_homeapp.py
+++ home/tests/tests_models_homeapp.py
@@ -1,5 +1,14 @@
-from home.models import Category
+from home.models import *
 
 
 def test_category_content(create_category):
+        category = Category.objects.all()
         assert create_category.name == 'TestNameCategory'
+        assert len(category) == 1
+
+def test_institution_content(create_institution):
+        institution = Institution.objects.all()
+        assert create_institution.name == 'TestNameInstitution'
+        assert create_institution.description == 'TestDescriptionInstitution'
+        assert create_institution.type == 1
+        assert len(institution) == 1

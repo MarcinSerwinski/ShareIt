ca8d6f3436dfab0608bc2d0388c9a15e2a643c54
MarcinSerwinski <MarcinSerwinski@github.com>
2023-01-11 20:09:20 +0100

    all kinds of institutions and its atributes can be displayed at landing-page

--- home/views.py
+++ home/views.py
@@ -1,9 +1,11 @@
+import random
+
 from django.db.models import Sum
 from django.shortcuts import render
 from django.views import View
 from django.views.generic import TemplateView, FormView, ListView
 
-from home.models import Donation, Institution
+from home.models import *
 
 
 class LandingPage(View):
@@ -11,8 +13,13 @@ class LandingPage(View):
     def get(self, request):
         sum_of_bags = Donation.objects.aggregate(Sum('quantity'))
         sum_of_institutions = Institution.objects.count()
+        fundations = Institution.objects.filter(type=1)
+        organizations = Institution.objects.filter(type=2)
+        local_charities = Institution.objects.filter(type=3)
         return render(request, 'home/index.html',
-                      {'sum_of_bags': sum_of_bags, 'sum_of_institutions': sum_of_institutions})
+                      {'sum_of_bags': sum_of_bags, 'sum_of_institutions': sum_of_institutions,
+                       'fundations': fundations, 'organizations': organizations, 'local_charities': local_charities})
+                       # 'a':a
 
 
 class AddDonation(TemplateView):

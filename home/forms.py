7e734b893d0cd8bb30cdf57129992471c814d380
MarcinSerwinski <MarcinSerwinski@github.com>
2023-01-19 18:30:20 +0100

    user can see message about passwords mismatch

--- home/forms.py
+++ home/forms.py
@@ -5,15 +5,15 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.core.exceptions import ValidationError
 
 class RegistrationForm(forms.ModelForm):
-    first_name = forms.CharField(max_length=128, widget=forms.TextInput(attrs={'class': 'form-group',
+    first_name = forms.CharField(max_length=128, widget=forms.TextInput(attrs={
                                                                                'placeholder': 'Imię'}), label='')
-    last_name = forms.CharField(max_length=128, widget=forms.TextInput(attrs={'class': 'form-group',
+    last_name = forms.CharField(max_length=128, widget=forms.TextInput(attrs={
                                                                               'placeholder': 'Nazwisko'}), label='')
-    email = forms.EmailField(max_length=128, widget=forms.EmailInput(attrs={'class': 'form-group',
+    email = forms.EmailField(max_length=128, widget=forms.EmailInput(attrs={
                                                                             'placeholder': 'Email'}), label='')
-    password1 = forms.CharField(max_length=128, widget=forms.PasswordInput(attrs={'placeholder': 'Hasło'}), label='Hasło')
+    password1 = forms.CharField(max_length=128, widget=forms.PasswordInput(attrs={'placeholder': 'Hasło'}), label='')
 
-    password2 = forms.CharField(max_length=128, widget=forms.PasswordInput(attrs={'placeholder': 'Powtórz hasło'}))
+    password2 = forms.CharField(max_length=128, widget=forms.PasswordInput(attrs={'placeholder': 'Powtórz hasło'}), label='')
 
     class Meta:
         model = get_user_model()
@@ -23,7 +23,7 @@ class RegistrationForm(forms.ModelForm):
         password1 = self.cleaned_data.get("password1")
         password2 = self.cleaned_data.get("password2")
         if password1 and password2 and password1 != password2:
-            raise ValidationError('')
+            raise ValidationError('Hasła nie są takie same')
         return password2
 
     def save(self, commit=True):

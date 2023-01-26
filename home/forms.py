6ea130270ea73faad9a4eabe7c46c93199b7e504
MarcinSerwinski <MarcinSerwinski@github.com>
2023-01-26 13:46:35 +0100

    will inherit django UserCreationForm for registration form

--- home/forms.py
+++ home/forms.py
@@ -5,7 +5,34 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.core.exceptions import ValidationError
 
 
-class RegistrationForm(forms.ModelForm):
+# class RegistrationForm(forms.ModelForm):
+#     first_name = forms.CharField(max_length=128, widget=forms.TextInput(attrs={'placeholder': 'Imię'}), label='')
+#     last_name = forms.CharField(max_length=128, widget=forms.TextInput(attrs={'placeholder': 'Nazwisko'}), label='')
+#     email = forms.EmailField(max_length=128, widget=forms.EmailInput(attrs={'placeholder': 'Email'}), label='')
+#     password1 = forms.CharField(max_length=128, widget=forms.PasswordInput(attrs={'placeholder': 'Hasło'}), label='')
+#     password2 = forms.CharField(max_length=128, widget=forms.PasswordInput(attrs={'placeholder': 'Powtórz hasło'}),
+#                                 label='')
+#
+#     class Meta:
+#         model = get_user_model()
+#         fields = ['email']
+#
+#     def clean_password2(self):
+#         password1 = self.cleaned_data.get("password1")
+#         password2 = self.cleaned_data.get("password2")
+#         if password1 and password2 and password1 != password2:
+#             raise ValidationError('Hasła nie są takie same')
+#         return password2
+#
+#     def save(self, commit=True):
+#         user = super().save(commit=False)
+#         user.set_password(self.cleaned_data.get('password'))
+#
+#         if commit:
+#             user.save()
+#
+#         return user
+class RegistrationForm(UserCreationForm):
     first_name = forms.CharField(max_length=128, widget=forms.TextInput(attrs={'placeholder': 'Imię'}), label='')
     last_name = forms.CharField(max_length=128, widget=forms.TextInput(attrs={'placeholder': 'Nazwisko'}), label='')
     email = forms.EmailField(max_length=128, widget=forms.EmailInput(attrs={'placeholder': 'Email'}), label='')
@@ -13,20 +40,14 @@ class RegistrationForm(forms.ModelForm):
     password2 = forms.CharField(max_length=128, widget=forms.PasswordInput(attrs={'placeholder': 'Powtórz hasło'}),
                                 label='')
 
+
     class Meta:
         model = get_user_model()
-        fields = ['email']
-
-    def clean_password2(self):
-        password1 = self.cleaned_data.get("password1")
-        password2 = self.cleaned_data.get("password2")
-        if password1 and password2 and password1 != password2:
-            raise ValidationError('Hasła nie są takie same')
-        return password2
+        fields = ['first_name', 'last_name', 'email', 'password1', 'password2']
 
     def save(self, commit=True):
-        user = super().save(commit=False)
-        user.set_password(self.cleaned_data.get('password'))
+        user = super(RegistrationForm, self).save(commit=False)
+        user.username = self.cleaned_data['email']
 
         if commit:
             user.save()
